11ae3a8ade1a958637212306e342349a6caf3a89
Ostyn Guerra <oguerra-2021270@kinal.edu.gt>
2023-10-09 15:22:33 -0600

    Email error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,17 @@ import { User } from './user';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+  export class AppComponent {
   topics = ['Angular', 'React', 'Vue'];
+  topicHasError= true;
 
-  userModel = new User('', 'rob@test.com', 55556464, '', 'morning', true);
+  userModel = new User('', 'rob@test.com', 55556464, 'default', 'morning', true);
+
+  validateTopic(value) {
+    if (value === 'default') {
+      this.topicHasError = true;
+    } else {
+      this.topicHasError = false;
+    }
+  }
 }
