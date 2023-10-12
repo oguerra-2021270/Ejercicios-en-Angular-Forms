ca9c5c5aa55a7d33b4d9d78aec1cc09dc3ae1e8e
Ostyn Guerra <oguerra-2021270@kinal.edu.gt>
2023-10-12 14:37:05 -0600

    FormBuilder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms'
+import { FormBuilder } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -7,16 +7,30 @@ import { FormGroup, FormControl } from '@angular/forms'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  registrationForm = new FormGroup({
-    userName: new FormControl('Ostyn'),
-    password: new FormControl(''),
-    confirmPassword: new FormControl(''),
-    address: new FormGroup({
-      city: new FormControl(''),
-      state: new FormControl(''),
-      postalCode: new FormControl('')
+
+  constructor(private fb: FormBuilder) {}
+
+  registrationForm = this.fb.group({
+    userName: ['Ostyn'],
+    password: [''],
+    confirmPassword: [''],
+    address: this.fb.group({
+      city: [''],
+      state: [''],
+      postalCode: ['']
     })
   });
+
+  // registrationForm = new FormGroup({
+  //   userName: new FormControl('Ostyn'),
+  //   password: new FormControl(''),
+  //   confirmPassword: new FormControl(''),
+  //   address: new FormGroup({
+  //     city: new FormControl(''),
+  //     state: new FormControl(''),
+  //     postalCode: new FormControl('')
+  //   })
+  // });
 
   loadApiData() {
     this.registrationForm.patchValue({
