3ec0e87377174aad806cf03d6cdfbf6e461da144
Ostyn Guerra <oguerra-2021270@kinal.edu.gt>
2023-10-13 15:12:14 -0600

    Conditional Validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms'
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 import { forbiddenNameValidator } from './shared/user-name.validator';
 import { PasswordValidator } from './shared/password-validator';
 
@@ -8,27 +8,50 @@ import { PasswordValidator } from './shared/password-validator';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  registrationForm?: FormGroup;
 
   get userName() {
-    return this.registrationForm.get('userName');
+    return this.registrationForm?.get('userName');
+  }
+
+  get email() {
+    return this.registrationForm?.get('email');
   }
 
   constructor(private fb: FormBuilder) {}
 
-  registrationForm = this.fb.group({
-    userName: ['', [Validators.required, Validators.minLength(3), forbiddenNameValidator(/password/)]] ,
-    password: [''],
-    confirmPassword: [''],
-    address: this.fb.group({
-      city: [''],
-      state: [''],
-      postalCode: ['']
-    })
-  }, {validator: PasswordValidator});
+  ngOnInit() {
+    this.registrationForm = this.fb.group({
+      userName: ['', [Validators.required, Validators.minLength(3), forbiddenNameValidator(/password/)]],
+      email: [''],
+      subscribe: [false],
+      password: [''],
+      confirmPassword: [''],
+      address: this.fb.group({
+        city: [''],
+        state: [''],
+        postalCode: ['']
+      })
+    }, {validator: PasswordValidator});
+
+    this.registrationForm.get('subscribe')?.valueChanges
+      .subscribe(checkedValue => {
+        const email = this.registrationForm?.get('email');
+        if (checkedValue) {
+          email?.setValidators(Validators.required);
+        } else {
+          email?.clearValidators
+        }
+        email?.updateValueAndValidity();
+      });
+
+  }
+
 
   loadApiData() {
-    this.registrationForm.patchValue({
+    this.registrationForm?.patchValue({
       userName: 'Bruce',
       password: 'test',
       confirmPassword: 'test'
